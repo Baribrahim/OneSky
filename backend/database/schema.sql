557ae4e29f0952760743e03072f626cf823c8b87
Abdul Bari <barii02@hotmail.com>
2025-10-01 14:36:42 +0100

    CREATE: created a schema file for our database

---
+++ backend/database/schema.sql
@@ -0,0 +1,77 @@
+drop database if exists oneskyv1; 
+
+create database oneskyv1;
+
+use oneskyv1;
+
+create table User
+	(
+	ID int primary key auto_increment,
+	Email varchar(100) not null,
+	Password varchar(100) not null,
+	FirstName varchar(100) not null,
+	LastName varchar(100) not null
+	);
+
+create table Cause
+	(
+    ID int primary key auto_increment,
+    Name varchar(255) not null,
+    Description varchar(1000) not null
+    );
+    
+create table `Event`
+	(
+    ID int primary key auto_increment,
+    CauseID int,
+    Foreign Key(CauseID) references Cause(ID),
+    Title varchar(255) not null,
+    About varchar(2000) not null,
+    Activities varchar(2000) not null,
+    Requirements varchar(2000) not null,
+    Schedule varchar(2000) not null,
+    ExpectedImpact varchar(2000) not null,
+    Date date not null,
+    StartTime time not null,
+    EndTime time not null,
+    Duration time not null,
+    LocationCity varchar(255) not null,
+    LocationPostcode varchar(10) not null,
+    Address varchar(255) not null,
+    Capacity int not null
+    );
+    
+Create table Tag
+	(
+    ID int primary key auto_increment,
+    TagName varchar(255) not null
+    );
+    
+create table CauseTag
+	(
+    TagID int,
+    Foreign Key(TagID) references Tag(ID),
+    CauseID int,
+    Foreign Key(CauseID) references Cause(ID)
+    );
+    
+create table EventRegistration
+	(
+    ID int primary key auto_increment,
+    EventID int,
+    Foreign Key(EventID) references Event(ID),
+    UserID int,
+    Foreign Key(UserID) references User(ID)
+    );
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
