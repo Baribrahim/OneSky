1f09d6039d88d8045cd0aef20e73472d7a98a474
schiv20 <schivaneed@outlook.com>
2025-10-03 15:27:09 +0100

    ADDED: events page

--- backend/functionality/app.py
+++ backend/functionality/app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, flash, jsonify, current_app, redirect, url_for
 from flask_cors import CORS
 from connector import Connector
 
@@ -9,6 +9,15 @@ CORS(app)
 @app.get("/")
 def home():
     return jsonify({"message": "Flask API is running", "routes": ["/health"]})
+
+@app.get('/events')
+def events():   
+    conn = Connector()
+    result = conn.extract_event_details()
+    inner = result[0]
+    print(inner[1])
+    print(inner[2])
+    return render_template('event.html', title='Events', event_title=inner[1], event_about=inner[2])
 
 @app.get("/health")
 def health():
