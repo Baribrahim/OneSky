da4dc365682619063202da38ea13890b96a932b6
Abdul Bari <barii02@hotmail.com>
2025-10-10 14:48:01 +0100

    CREATE: styling for auth

--- frontend/src/components/SkyBrand.jsx
+++ frontend/src/components/SkyBrand.jsx
@@ -4,7 +4,9 @@ import logo from "../assets/OneSky-logo.png";
 export default function SkyBrand({ size = 32 }) {
   return (
     <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
-      <img src={logo} alt="OneSky" style={{ height: size }} />
+      <div className="logo-center">
+        <img src={logo} alt="OneSky Logo" style={{ height: size }} />
+      </div>
       <span className="brand-gradient" style={{ fontSize: size * 0.6, fontWeight: 700 }}>
       </span>
     </div>
