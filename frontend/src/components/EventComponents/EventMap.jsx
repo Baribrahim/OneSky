ef0ec86b2f779e79b9e3be69b917209b387e4adb
Reanna-Sky <reannagibsonsky@gmail.com>
2025-11-01 12:58:19 +0000

    CREATE: created the map component

---
+++ frontend/src/components/EventComponents/EventMap.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../styles/events.css";
+
+function EventMap({ latitude, longitude }) {
+  if (!latitude || !longitude) {
+    return <p>Loading map...</p>;
+    
+  }
+  
+    console.log("Map Props:", latitude, longitude);
+
+  const mapSrc = `https://www.google.com/maps?q=${latitude},${longitude}&z=14&output=embed`;
+
+  return (
+    <div className="event-section">
+        <h2>Map</h2>
+        <div style={{ marginTop: "20px" }}>
+          <iframe
+            title="Event Location"
+            src={mapSrc}
+            width="50%"
+            height="400"
+            style={{ borderRadius: "10px", border: "none" }}
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
+        </div>
+    </div>
+  );
+}
+
+export default EventMap;
+
