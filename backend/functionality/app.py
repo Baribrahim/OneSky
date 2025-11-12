a8f2154d0de56d79f92cd68ba549fb13a76c3b40
schiv20 <schivaneed@outlook.com>
2025-11-12 16:01:03 +0000

    UPDATE: app.py to run in prod

--- backend/functionality/app.py
+++ backend/functionality/app.py
@@ -89,4 +89,4 @@ if __name__ == "__main__":
     )
 
     # IMPORTANT: run with socketio, not app.run
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5001, debug=False, allow_unsafe_werkzeug=True)
